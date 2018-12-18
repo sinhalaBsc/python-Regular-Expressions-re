c4732958908455e207f5fd69140ecf1996dd05b7
samadhi laksahan <35246020+sinhalaBsc@users.noreply.github.com>
2018-12-18 14:03:42 +0300

    print keyboard special characters

---
+++ keyboard special characters.py
@@ -0,0 +1,63 @@
+# This library should import for work with regular expression
+import re
+
+# you can use following syntec for multiple line in python.it's call 'multiline string'
+# ''' my multiline string'''
+
+
+text_to_search='''
+abcdefghijklmnopqrstuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+'''
+txt='''
+~ 	Tilde.
+` 	Acute, back quote, grave, grave accent, left quote, open quote, or a push.
+! 	Exclamation mark, exclamation point, or bang.
+@ 	Ampersat, arobase, asperand, at, or at symbol.
+# 	Octothorpe, number, pound, sharp, or hash.
+£ 	Pound Sterling or Pound symbol.
+€ 	Euro.
+$ 	Dollar sign or generic currency.
+¢ 	Cent sign.
+¥ 	Chinese/Japenese Yuan.
+§ 	Micro or Section.
+% 	Percent.
+° 	Degree.
+^ 	Caret or circumflex.
+& 	Ampersand, epershand, or and symbol.
+* 	Asterisk, mathematical multiplication symbol, and sometimes referred to as star.
+( 	Open parenthesis.
+) 	Close parenthesis.
+- 	Hyphen, minus or dash.
+_ 	Underscore.
++ 	Plus.
+= 	Equal.
+{ 	Open brace, squiggly brackets, or curly bracket.
+} 	Close brace, squiggly brackets, or curly bracket.
+[ 	Open bracket.
+] 	Closed bracket.
+| 	Pipe, or, or vertical bar.
+\ 	Backslash or reverse solidus.
+/ 	Forward slash, solidus, virgule, whack, and mathematical division symbol.
+: 	Colon.
+; 	Semicolon.
+" 	Quote, quotation mark, or inverted commas.
+' 	Apostrophe or single quote.
+< 	Less than or angle brackets.
+> 	Greater than or angle brackets.
+, 	Comma.
+. 	Period, dot or full stop.
+? 	Question mark.'''
+
+
+al=''
+p=0
+for i in txt:
+    p+=1
+    if  i=='\n':
+        al+=txt[p]
+        #print(txt[p])
+
+print(al)
+
